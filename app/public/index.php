946bbc1dc94a014f5d38a021e93059bd001a2c25
Arold Mauger <arold.mauger@mary.fr>
2024-10-02 13:51:38 +0200

    Installation de Twig, factorisation du header, footer et oeuvres sur index.php

--- app/public/index.php
+++ app/public/index.php
@@ -1,7 +1,13 @@
 <?php
 
-echo("test");
+global $oeuvres;
+require_once '../vendor/autoload.php';
+require_once 'oeuvres.php';
 
+$loader = new \Twig\Loader\FilesystemLoader('/');
+$twig = new \Twig\Environment($loader);
 
-// require_once  des data depuis le fichier oeuvres.php / ou controller
-// require_once du vendor
+$template = $twig->load('index.html.twig');
+echo $template->render(['oeuvres' => $oeuvres]);
+
+?>
